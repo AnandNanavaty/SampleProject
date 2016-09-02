20c3f0434da34006e784c79a1d8d2b1fe876a289
indianic <indianic@ind125.local>
2016-09-02 16:27:55 +0530

    initial commit

---
+++ MyFramework/MyFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyFramework.h
+//  MyFramework
+//
+//  Created by indianic on 02/09/16.
+//  Copyright © 2016 indianic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFramework.
+FOUNDATION_EXPORT double MyFrameworkVersionNumber;
+
+//! Project version string for MyFramework.
+FOUNDATION_EXPORT const unsigned char MyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework/PublicHeader.h>
+
+
---
+++ MyFramework/myCustomView.swift
@@ -0,0 +1,22 @@
+//
+//  myCustomView.swift
+//  AnandFramework
+//
+//  Created by indianic on 02/09/16.
+//  Copyright © 2016 indianic. All rights reserved.
+//
+
+import UIKit
+
+public class myCustomView: UIView {
+    
+    public  init(frame: CGRect) {
+        
+    }
+    
+    public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+}
